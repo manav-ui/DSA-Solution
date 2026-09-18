7db9c1d137355c96acca82be088a49d27e9ef941
Manav <manavsati176@gmail.com>
2026-09-18 10:38:52 +0530

    done

---
+++ Codeforces/khba_friends.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/contest/2167/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--){
+        int n, k , x;
+        cin >> n >> k >> x;
+        vector<int> post(n);
+        for(auto &it : post){
+            cin >> it;
+        }
+    }
+    return 0;
+}
---
+++ Codeforces/queries.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/600/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n), b(m);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < m; i++) {
+        int count = upper_bound(a.begin(), a.end(), b[i]) - a.begin();
+        cout << count << " ";
+    }
+
+    return 0;
+}
